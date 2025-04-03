1c272f3406e8d3c29f8a6ca732573d1688a93863
venypon <paulnvene@yahoo.com>
2025-04-03 16:27:53 +0000

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+# This is a simple Python program
+
+# Your program begins with a call to main()
+# Prints "Hello, World" to the terminal window.
+def main():
+    print("Hello, World")
+
+if __name__ == "__main__":
+    main()
